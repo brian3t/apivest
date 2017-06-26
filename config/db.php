9c7ccd46d42f81f5ff361001a7f5e75c2b2eadff
Brian Nguyen <ngxtri@gmail.com>
2017-06-25 22:11:27 -0700

    Initial

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=vest',
+    'username' => 'vest',
+    'password' => 'vTrapok)1',
+    'charset' => 'utf8',
+];
