9fb037f4041a86221a997d0e22c90475fbd1163b
Brian Nguyen <ngxtri@gmail.com>
2017-07-18 23:25:39 -0700

    Cron hermes pull nasdaq done

--- commands/HermesController.php
+++ commands/HermesController.php
@@ -17,17 +17,18 @@ class HermesController extends Controller
      */
     public function actionPullNasdaq()
     {
-        echo "Pulling";
+        echo "Pulling" . PHP_EOL;
         $ch = curl_init();
 
-        curl_setopt($ch, CURLOPT_URL, "http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download");
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch,CURLOPT_URL,"http://www.nasdaq.com/screening/companies-by-industry.aspx?exchange=NASDAQ&render=download");
+        curl_setopt($ch,CURLOPT_HEADER,0);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 
         $company_list = curl_exec($ch);
-        $company_list_lines = explode(PHP_EOL, $company_list);
+        $company_list_lines = explode(PHP_EOL,$company_list);
         $symbols = [];
-        foreach ($company_list_lines as $company_list_line) {
+        foreach ($company_list_lines as $company_list_line)
+        {
             $symbols[] = str_getcsv($company_list_line);
         }
         curl_close($ch);
@@ -39,18 +40,24 @@ class HermesController extends Controller
         $sector = $industry = $summary_quote = '';
         $db_command = $db->createCommand("REPLACE INTO vest.stock(symbol, `name`, last_sale, market_cap, ipo_year, sector, industry, summary_quote, country) 
 VALUES (:symbol, :name, :last_sale, :market_cap,:ipo_year,:sector,:industry,:summary_quote,'us')")
-            ->bindValues([':symbol' => $symbol, ':name' => $name, ':last_sale' => $last_sale, ':market_cap' => $market_cap, ':ipo_year' => $ipo_year, ':sector' => $sector, ':industry' => $industry, ':summary_quote' => $summary_quote]);
+            ->bindParam(':symbol',$symbol)->bindParam(':name',$name)->bindParam(':last_sale',$last_sale)
+            ->bindParam(':market_cap',$market_cap)->bindParam(':ipo_year',$ipo_year)
+            ->bindParam(':sector',$sector)->bindParam(':industry',$industry)->bindParam(':summary_quote',$summary_quote);
         $headers = array_shift($symbols);
-        foreach ($symbols as $s){
+        $rows_added = 0;
+        foreach ($symbols as $s)
+        {
+            if(count($s)!==10) continue;
             list($symbol,$name,$last_sale,$market_cap,$adr_tso,$ipo_year,$sector,$industry,$summary_quote) = $s;
-            try {
+            try
+            {
                 $result = $db_command->query();
-                var_dump($result);
-                how to do bindvalues?
-                $a=1;
-            } catch (Exception $exception){
+                $rows_added += $result->rowCount / 2;
+            } catch (Exception $exception)
+            {
                 \Yii::error("Cant pull nasdaq: " . $exception->getMessage());
             }
         }
+        echo "$rows_added rows added";
     }
 }
